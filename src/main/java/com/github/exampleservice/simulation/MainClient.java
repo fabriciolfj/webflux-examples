2e0399763ef7b6af5425d4d69e28e3fcbee16286
spark <fabricio.jacob@outlook.com>
2023-06-05 21:38:04 -0300

    add

--- src/main/java/com/github/exampleservice/simulation/MainClient.java
+++ src/main/java/com/github/exampleservice/simulation/MainClient.java
@@ -4,15 +4,20 @@ import com.github.exampleservice.entities.Member;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class MainClient {
 
-    public static void main(String[] args) {
-        var url = "http://127.0.0.1:8080";
+    public static void main(String[] args) throws IOException {
+        var url = "http://localhost:8080";
         WebClient.create(url)
                 .get()
                 .uri("/api/v1/members")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchangeToFlux(c -> c.bodyToFlux(Member.class))
                 .subscribe(System.out::println);
+
+        System.in.read();
     }
 }
