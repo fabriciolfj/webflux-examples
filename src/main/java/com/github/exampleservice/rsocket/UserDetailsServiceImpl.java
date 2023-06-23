83e70ad53b43ae2fec4c5ffb01d9df1f365506b4
spark <fabricio.jacob@outlook.com>
2023-06-22 23:08:28 -0300

    add

---
+++ src/main/java/com/github/exampleservice/rsocket/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.github.exampleservice.rsocket;
+
+import com.github.exampleservice.security.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class UserDetailsServiceImpl implements ReactiveUserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Mono<UserDetails> findByUsername(String username) {
+        return Mono.fromSupplier(() -> this.userRepository.findByUSerName(username));
+    }
+}
